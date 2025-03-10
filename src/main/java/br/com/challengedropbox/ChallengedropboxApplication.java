6df8c38f612f58ea6bf2c1cc2cea71b8b02f150e
emersonlourenzi <emersonl.lourenzi@gmail.com>
2025-03-10 11:22:15 -0300

    initial config

---
+++ src/main/java/br/com/challengedropbox/ChallengedropboxApplication.java
@@ -0,0 +1,13 @@
+package br.com.challengedropbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengedropboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengedropboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/challengedropbox/ChallengedropboxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.challengedropbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengedropboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
