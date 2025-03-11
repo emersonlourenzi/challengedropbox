1da9fa40f512b5e70f3c82c89bb5034daff589a7
emersonlourenzi <emersonl.lourenzi@gmail.com>
2025-03-11 14:42:58 -0300

    Cria fluxo de deletar usuário por email

--- src/main/java/br/com/challengedropbox/repository/user/UserRepository.java
+++ src/main/java/br/com/challengedropbox/repository/user/UserRepository.java
@@ -1,6 +1,7 @@
 package br.com.challengedropbox.repository.user;
 
 import br.com.challengedropbox.repository.user.entity.UserEntity;
+import jakarta.validation.Valid;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,7 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends MongoRepository<UserEntity, String> {
 
     UserEntity findByEmail(String email);
+
+    void deleteByEmail(@Valid String email);
 
 }
